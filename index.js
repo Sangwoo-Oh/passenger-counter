3860215cbeae3ec361c7e3bce4b4766a843d2aa8
Sangwoo-Oh <sangwoooh1013@gmail.com>
2024-04-14 11:16:33 +0200

    add event listern

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+// adding event listener
+let incrementButton = document.getElementById("increment-btn").addEventListener("click", increment)
+let saveButton = document.getElementById("save-btn").addEventListener("click", save)
 let countEl = document.getElementById("count")
 let saveEl = document.getElementById("save-el")
 
